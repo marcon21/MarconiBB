e76494e2e27e2e4e4b62225891807a9deb6a6651
marcon21 <marcon859@gmail.com>
2019-10-03 11:47:21 +0200

    Created template using docker and flask, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # MarconiBB
-Marconi Badge Booking
+
+## Marconi Badge Booking
+
+## Created by @marcon21, @strambinienea, @giacomotezza
+
+---
+
+#### Requirements:
+
+- Dokcer + docker-compose
+
+#### For running the program please run `docker-compose up` and go to 'http:localhost:5000'
---
+++ flask/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, escape, request, redirect, send_from_directory
+import pugsql
+
+app = Flask(__name__)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html', error_message="Page not found")
+
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css', path)
+
+
+@app.route('/images/<path:path>')
+def send_image(path):
+    return send_from_directory('images', path)
+
+
+@app.route('/home')
+def home():
+    page = {
+        title: "Home"
+    }
+    return render_template('home.html', page=page)
+
+
+def root():
+    return redirect("/home")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
