0c91ba10d47b5a91158b84fbd020324c92742002
Daniel Marcon <marcon859@gmail.com>
2019-12-05 10:14:59 +0100

    Modified ButtonsDeamon

--- ButtonsDeamon.py
+++ ButtonsDeamon.py
@@ -1,8 +1,16 @@
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#PIN BOTTONI:
 
-while True: # Run forever
-    if GPIO.input(10) == GPIO.HIGH:
-        print("Button was pushed!")
+BOTTONE_1 = 22
+BOTTONE_2 = 24
+BOTTONE_3 = 26
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(BOTTONE_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BOTTONE_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BOTTONE_3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.add_event_detect(BOTTONE_1, GPIO.RISING, callback=buttonPressed)
+GPIO.add_event_detect(BOTTONE_2, GPIO.RISING, callback=buttonPressed)
+GPIO.add_event_detect(BOTTONE_3, GPIO.RISING, callback=buttonPressed)
