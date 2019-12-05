0402503e7ecd14b4e0eaf4d4709de85095f67002
Daniel Marcon <marcon859@gmail.com>
2019-12-05 10:51:49 +0100

    bug fixing in ButtonsDeamon

--- ButtonsDeamon.py
+++ ButtonsDeamon.py
@@ -1,16 +1,37 @@
 import RPi.GPIO as GPIO
+import requests
 
-#PIN BOTTONI:
+#PIN BOTTONI
+BUTTON_1 = 22
+BUTTON_2 = 24
+BUTTON_3 = 26
 
-BOTTONE_1 = 22
-BOTTONE_2 = 24
-BOTTONE_3 = 26
+def button1Pressed(channel):
+    print("Tasto 1 premuto")
+
+    if GPIO.input(BUTTON_1) == GPIO.HIGH:
+        print(requests.post('http://localhost:5000/button1'))
+
+def button2Pressed(channel):
+    print("Tasto 2 premuto")
+
+    if GPIO.input(BUTTON_2) == GPIO.HIGH:
+        print(requests.post('http://localhost:5000/button2'))
+
+def button3Pressed(channel):
+    print("Tasto 3 premuto")
+
+    if GPIO.input(BUTTON_3) == GPIO.HIGH:
+        print(requests.post('http://localhost:5000/button3'))
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(BOTTONE_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(BOTTONE_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(BOTTONE_3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BUTTON_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BUTTON_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BUTTON_3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(BOTTONE_1, GPIO.RISING, callback=buttonPressed)
-GPIO.add_event_detect(BOTTONE_2, GPIO.RISING, callback=buttonPressed)
-GPIO.add_event_detect(BOTTONE_3, GPIO.RISING, callback=buttonPressed)
+GPIO.add_event_detect(BUTTON_1, GPIO.RISING, callback=button1Pressed)
+GPIO.add_event_detect(BUTTON_2, GPIO.RISING, callback=button2Pressed)
+GPIO.add_event_detect(BUTTON_3, GPIO.RISING, callback=button3Pressed)
+
+while True:
+    pass
