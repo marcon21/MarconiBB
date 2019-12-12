15beccd7030051090354df4ed997ca6ba06f808e
Daniel Marcon <marcon859@gmail.com>
2019-12-12 08:26:52 +0100

    Update to ButtonsDeamon

--- ButtonsDeamon.py
+++ ButtonsDeamon.py
@@ -10,19 +10,19 @@ def button1Pressed(channel):
     print("Tasto 1 premuto")
 
     if GPIO.input(BUTTON_1) == GPIO.HIGH:
-        print(requests.post('http://localhost:5000/button1'))
+        print(requests.get('http://localhost:5000/button1'))
 
 def button2Pressed(channel):
     print("Tasto 2 premuto")
 
     if GPIO.input(BUTTON_2) == GPIO.HIGH:
-        print(requests.post('http://localhost:5000/button2'))
+        print(requests.get('http://localhost:5000/button2'))
 
 def button3Pressed(channel):
     print("Tasto 3 premuto")
 
     if GPIO.input(BUTTON_3) == GPIO.HIGH:
-        print(requests.post('http://localhost:5000/button3'))
+        print(requests.get('http://localhost:5000/button3'))
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BUTTON_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
